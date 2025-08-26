becdc1dbf1709f43db06ebc008434f76305bacbe
Skulldorom <51134009+Skulldorom@users.noreply.github.com>
2025-08-26 19:54:36 +0300

    update

--- src/index.js
+++ src/index.js
@@ -397,7 +397,7 @@ const SessionManagerProvider = ({
                 width: "40px",
                 height: "40px",
                 border: "4px solid rgba(0,0,0,0.1)",
-                borderTop: "4px solid #c51010ff",
+                borderTop: "4px solid #3498db",
                 borderRadius: "50%",
                 animation: "spin 1s linear infinite",
               }}
