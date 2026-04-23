c179a86358deac13e413af36a460b8dd01a7f391
Skulldorom <51134009+Skulldorom@users.noreply.github.com>
2026-04-23 11:45:39 +0300

    fixed css not working

--- webpack.config.js
+++ webpack.config.js
@@ -30,9 +30,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: {
-          loader: "css-loader",
-        },
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
