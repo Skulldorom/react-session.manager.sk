6808d79f7735571f6b59d8af3a045b0b643a3c1b
Skulldorom <51134009+Skulldorom@users.noreply.github.com>
2026-06-09 11:30:41 +0300

    Changes to fingerprint JS

--- src/components/FingerPrint.js
+++ src/components/FingerPrint.js
@@ -1,11 +1,14 @@
-import { ClientJS } from "clientjs";
+import FingerprintJS from "@fingerprintjs/fingerprintjs";
 
 export default function getDeviceFingerprint() {
   if (localStorage.getItem("deviceFingerprint"))
     return localStorage.getItem("deviceFingerprint");
 
-  const client = new ClientJS();
-  const fingerprint = client.getFingerprint();
+  const fpPromise = FingerprintJS.load();
+  const fingerprint = fpPromise
+    .then((fp) => fp.get())
+    .then((result) => result.visitorId);
+
   localStorage.setItem("deviceFingerprint", fingerprint);
   return fingerprint;
 }
